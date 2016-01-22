90e251b79c63459c4443f89102bb369a48af515f
Marcus Kida <marcus@kida.io>
2016-01-22 20:02:24 +1100

    Implement nginx config generation and reload

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var getRawBody = require('raw-body');
 var shortid = require('shortid');
 var Promise = require('bluebird');
+var format = require("string-template")
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
+var exec = require('child_process').exec;
 
 router.post('/', function (req, res, next) {
 
@@ -14,7 +18,8 @@ router.post('/', function (req, res, next) {
   ];
 
   var aContainer;
-
+  var aName;
+  var aPort;
   return req.swifton.docker.createContainerAsync({
     Image: 'swiftdocker/swift',
     Cmd: commands,
@@ -38,21 +43,64 @@ router.post('/', function (req, res, next) {
     return aContainer.startAsync();
   })
   .then(function (data) {
-    console.log('data1', data);
     return aContainer.inspectAsync();
   })
   .then(function (data) {
-    console.log('data2', data);
-    console.log('host port:', data["NetworkSettings"]["Ports"]["8000/tcp"][0]["HostPort"]);
+    aPort = data["NetworkSettings"]["Ports"]["8000/tcp"][0]["HostPort"];
+    aName = data["Name"].replace('/','').replace('_','-');
+    return createNginxConfig(aName, aPort);
+  })
+  .then(function (nginxConfig) {
+    return fs.writeFileAsync(
+      path.join(__dirname, '../', 'vhosts', aContainer.id + '.conf'),
+      nginxConfig
+    );
+  })
+  .then(function () {
+    return execute('service nginx reload');
+  })
+  .then(function (out, code) {
+    console.log('out', out);
+    console.log('code', code);
     res.json({
       success: true,
-      container_id: aContainer.id
+      container_id: aContainer.id,
+      service_uri: aName + '.serve.swifton.me'
     });
-  })
+  });
 });
 
-function getFilename (req) {
-  return (typeof req.params.filename !== 'undefined') ? req.params.filename + ".swift" : shortid.generate() + ".swift";
+var createNginxConfig = function createNginxConfig (name, port, callback) {
+  return new Promise(function (resolve, reject) {
+    var template = 'server {\n' +
+      'listen 80;\n' +
+      'server_name {name}.serve.swifton.me;\n' +
+      'access_log /var/log/nginx/{name}.serve.swifton.me.access.log;\n' +
+      'error_log /var/log/nginx/{name}.serve.swifton.me.error.log;\n' +
+      'location / {\n' +
+        'proxy_pass http://172.17.0.1:{port};\n'+
+        'proxy_set_header Upgrade $http_upgrade;\n' +
+        'proxy_set_header Connection "upgrade";\n' +
+        'proxy_http_version 1.1;\n' +
+      '}\n' +
+    '}';
+    var config = format(template, {
+      name: name,
+      port: port
+    });
+    return resolve(config);
+  });
+};
+
+var execute = function execute(command){
+  return new Promise(function (resolve, reject) {
+    exec(command, function(error, stdout, stderr) {
+      if (error) {
+        return reject(error);
+      }
+      resolve(stdout);
+    });
+  });
 };
 
 module.exports = router;
