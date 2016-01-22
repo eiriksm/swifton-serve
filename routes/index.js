ebb372d6603ba208e5316661f4e2857144404d5d
Marcus Kida <marcus@kida.io>
2016-01-22 11:52:30 +1100

    Further docker experiments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var getRawBody = require('raw-body');
 var shortid = require('shortid');
 var Promise = require('bluebird');
-var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,72 +15,77 @@ function handleServe (req, res, next) {
   var repository = req.body.repository;
 
   var commands = [
-    'bash',
-    '-c',
-    'git clone ' + repository + ' app && cd app && swift build --configuration release && cd .build/release && exe=$(grep -o \'name: "[^"]*"\' ../../Package.swift | sed \'s/name: "//g\' | sed \'s/"//g\') && ./$exe'
+    '/bin/bash -c git clone ' + repository + ' app && cd app && swift build --configuration release && cd .build/release && exe=$(grep -o \'name: "[^"]*"\' ../../Package.swift | sed \'s/name: "//g\' | sed \'s/"//g\') && ./$exe'
   ];
 
-  // req.swifton.docker.listContainers(function (err, containers) {
-  //   console.log('containers:', containers);
-  // });
-  //
-  // var opts = {
-  //   HostConfig: {
-  //     PublishAllPorts: true,
-  //     Privileged: true
-  //   }
-  // };
-
-  // req.swifton.docker.run('swiftdocker/swift', commands, process.stdout, opts, opts, function (err, data,container) {
-  //   console.log('data', data);
-  //   console.log('container', container);
-  //   res.json(data);
-  // })
-  // .on('stream', function (stream) {
-  //   // console.log('stream', stream);
-  //   stream.on('data', function (data) {
-  //     console.log('\nstream data >>>', data.toString(), '\n');
-  //   });
-  //   stream.on('container', function (container) {
-  //     console.log('\nstream container >>>', container.toString(), '\n');
-  //   });
-  // });
-
-  var theContainer = null;
-  req.swifton.docker.createContainerAsync({
+  req.swifton.docker.createContainer({
     Image: 'swiftdocker/swift',
-    // Cmd: commands,
-    HostConfig: {
-      PublishAllPorts: true,
-      Privileged: true
-    }
-  })
-  .then(function (container) {
-    console.log('CREATE CONTAINER:', container);
-    theContainer = container;
-    return Promise.promisifyAll(container).startAsync();
-  })
-  .then(function (exec) {
-    return theContainer.execAsync({
-      Cmd: commands,
-      AttachStdin: true,
-      AttachStdout: true
-    });
-  })
-  .then(function (data) {
-    console.log('CONTAINER EXEC:', exec);
-  })
-  .error(function (err) {
-    console.log('ERROR:', err);
-  });
-};
+  }, function (err, container) {
+    container.start({
+      Cmd: commands
+    }, function (err, data) {
+      console.log('exec \'in:', commands);
+      console.log('exec \'err:', err);
+      console.log('exec \'data:', data);
 
-function send (res, out, err) {
-  console.log('send typeof out', typeof out);
-  console.log('send out', out);
-  console.log('send typeof err', typeof err);
-  console.log('send err', err);
-  res.send(out ? out : err);
+      // container.exec({
+      //   // AttachStdin: true,
+      //   // AttachStdout: true,
+      //   Cmd: commands
+      // }, function (err, exec) {
+      //   console.log('exec err', err);
+      //   exec.start({
+      //     hijack: true,
+      //     stdin: true
+      //   }, function (err, stream) {
+      //     stream.pipe(process.stdout);
+      //     console.log('err', err);
+      //     // console.log('stream', stream);
+      //   });
+      // });
+    });
+  });
+
+//   var theContainer = null;
+//   req.swifton.docker.createContainerAsync({
+    // Image: 'swiftdocker/swift',
+    // // Cmd: commands,
+    // HostConfig: {
+    //   PublishAllPorts: true,
+    //   Privileged: true
+    // }
+//   })
+//   .then(function (container) {
+//     console.log('CREATE CONTAINER:', container);
+//     theContainer = container;
+//     return Promise.promisifyAll(container).startAsync();
+//   })
+//   .then(function (exec) {
+//
+//     return Promise.promisifyAll(theContainer).execAsync({
+//       Cmd: commands,
+//       AttachStdin: true,
+//       AttachStdout: true
+//     });
+//   })
+//   .then(function (data) {
+//     console.log('CONTAINER EXEC:', data);
+//     // exec(commands, function (e,c) {
+//       // console.log('EXEC E:', e);
+//       // console.log('EXEC C:', c);
+//     // });
+//   })
+//   .error(function (err) {
+//     console.log('ERROR:', err);
+//   });
+// };
+//
+// function send (res, out, err) {
+//   console.log('send typeof out', typeof out);
+//   console.log('send out', out);
+//   console.log('send typeof err', typeof err);
+//   console.log('send err', err);
+//   res.send(out ? out : err);
 };
 
 function getFilename (req) {
